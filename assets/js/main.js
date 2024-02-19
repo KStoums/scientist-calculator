db6c2c71a0a60b5fec311a923936f6f00940b8f5
Kstoums <kstoums@gmail.com>
2024-02-19 15:58:40 +0100

    Refactor all functions in one

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 let calculation = [];
 let result = "";
 
+const operation = {
+    "hex":math.hex,
+    "dec":math.floor,
+    "oct":math.oct,
+    "bin":math.bin,
+    "deg":math.deg,
+    "asin":math.asin,
+    "acos":math.acos,
+    "atan":math.atan,
+    "sin":math.sin,
+    "cos":math.cos,
+    "tan":math.tan,
+    "sqrt":math.sqrt
+}
+
 const calculationElement = document.getElementById("calculation");
 const resultElement = document.getElementById("result");
 
@@ -34,152 +49,32 @@ function calculate() {
     try {
         result = math.evaluate(calculationString);
         resultElement.textContent = result;
+        return result;
     } catch (error) {
         sendMessage("ERROR");
+        return "0";
     }
 }
 
-function hexCalculate() {
+function specialCalculate(calculateType) {
     let calculationString = calculation.join("");
 
-    try {
-        result = math.hex(calculationString);
-        resultElement.textContent = result.substring(2);
-    } catch (error) {
-        sendMessage("ERROR");
+    if (calculateType === "sqrt" || calculateType === "asin" || calculateType === "acos" ||
+        calculateType === "atan" || calculateType === "sin" || calculateType === "cos" || calculateType === "tan") {
+        const calculateString = calculate();
+        try {
+            result = operation[calculateType](calculateString);
+            resultElement.textContent = result;
+            calculationElement.textContent = calculateType + "(" + calculationString + ")"
+        } catch (error) {
+            sendMessage("ERROR");
+        }
+        return;
     }
-}
-
-function decCalculate() {
-    let calculationString = calculation.join("");
 
     try {
-        result = math.floor(calculationString);
+        result = operation[calculateType](calculationString);
         resultElement.textContent = result;
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function octCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        result = math.oct(calculationString);
-        resultElement.textContent = result.substring(2);
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function binCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        result = math.bin(calculationString);
-        resultElement.textContent = result.substring(2);
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function degCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        result = math.tan(calculationString);
-        resultElement.textContent = result;
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function sqrtCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        const calculationResult = math.evaluate(calculationString);
-        result = math.sqrt(calculationResult);
-        resultElement.textContent = result;
-        calculationElement.textContent = "√("+ calculationString + ")"
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function asinCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        const calculationResult = math.evaluate(calculationString);
-        result = math.asin(calculationResult);
-        resultElement.textContent = result;
-        calculationElement.textContent = "asin("+ calculationString + ")"
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function acosCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        const calculationResult = math.evaluate(calculationString);
-        result = math.acos(calculationResult);
-        resultElement.textContent = result;
-        calculationElement.textContent = "acos("+ calculationString + ")"
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function atanCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        const calculationResult = math.evaluate(calculationString);
-        result = math.atan(calculationResult);
-        resultElement.textContent = result;
-        calculationElement.textContent = "atan("+ calculationString + ")"
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function sinCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        const calculationResult = math.evaluate(calculationString);
-        result = math.sin(calculationResult);
-        resultElement.textContent = result;
-        calculationElement.textContent = "sin("+ calculationString + ")"
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function cosCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        const calculationResult = math.evaluate(calculationString);
-        result = math.cos(calculationResult);
-        resultElement.textContent = result;
-        calculationElement.textContent = "cos("+ calculationString + ")"
-    } catch (error) {
-        sendMessage("ERROR");
-    }
-}
-
-function tanCalculate() {
-    let calculationString = calculation.join("");
-
-    try {
-        const calculationResult = math.evaluate(calculationString);
-        result = math.tan(calculationResult);
-        resultElement.textContent = result;
-        calculationElement.textContent = "tan("+ calculationString + ")"
     } catch (error) {
         sendMessage("ERROR");
     }
